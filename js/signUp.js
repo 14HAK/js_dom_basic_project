8edc651b8e09b7b4022342c1e65fb564cc6b3792
Dulon Mahadi <dulonmolla88@gmail.com>
2022-08-23 03:26:29 +0600

    githum www online change

--- js/signUp.js
+++ js/signUp.js
@@ -9,8 +9,7 @@ User_btn.addEventListener('click', () => {
   const email = user_email.value;
   const password = user_password.value;
 
-  if (email === 'dulonmolla88@gmail.com' && password === 'dulonmolla88') {
-//    location.href = 'http://127.0.0.1:5500/bank.html'
+  if (email === 'dulonmolla88@gmail.com' && password === '1234') {
       location.href = 'https://14hak.github.io/js_dom_basic_project/bank.html'
   } else {
     alert('তুই আমার ছেলে না। 😡');
